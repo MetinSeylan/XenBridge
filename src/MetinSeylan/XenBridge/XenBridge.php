e6f7c06b4b647c68f0d825bae1e2503fed30c534
Metin Seylan <metinsyln@gmail.com>
2013-12-08 22:44:59 +0200

    session update

--- src/MetinSeylan/XenBridge/XenBridge.php
+++ src/MetinSeylan/XenBridge/XenBridge.php
@@ -5,6 +5,7 @@ namespace MetinSeylan\XenBridge;
 use Illuminate\Support\Facades\Config;
 use XenForo_Autoloader;
 use XenForo_Application;
+use XenForo_Session;
 use XenForo_Visitor;
 use XenForo_Model;
 
@@ -13,6 +14,7 @@ class XenBridge {
     static function boot() {
 
         $lib = $_SERVER['DOCUMENT_ROOT'] . '/' . Config::get('XenBridge::config.xenforoFolder') . '/library';
+    
         require($lib . '/XenForo/Autoloader.php');
         
         XenForo_Autoloader::getInstance()->setupAutoloader($lib);
@@ -20,22 +22,10 @@ class XenBridge {
         XenForo_Application::disablePhpErrorHandler();
 
         if (Config::get('XenBridge::config.autoLogin'))
-            self::_setSession();
+            XenForo_Session::startPublicSession();
+
     }
 
-    private static function _setSession() {
-        if (isset($_COOKIE['xf_session'])) {
-
-            $xf_session = self::getDb()->fetchRow('select * from xf_session where session_id = ? and expiry_date > ?', array($_COOKIE['xf_session'], time()));
-            if ($xf_session) {
-                $data = unserialize($xf_session['session_data']);
-                if (isset($data['user_id'])) {
-                    // Xenforo set visitor
-                    XenForo_Visitor::setup($data['user_id']);
-                }
-            }
-        }
-    }
 
     public function getDb() {
         return XenForo_Application::getDb();
